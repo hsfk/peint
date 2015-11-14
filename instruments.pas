feeb8877f84e4328609636fcc3d6d3148f43a069
hsfk <hsfk@yandex.ru>
2015-11-14 19:24:17 +1000

    ctrlz ficha improvment

--- instruments.pas
+++ instruments.pas
@@ -5,109 +5,235 @@ unit Instruments;
 interface
 
 uses
-  Classes, SysUtils,Graphics,dialogs;
+  Classes, SysUtils, Graphics, Dialogs;
 
 type
   main = class
-    PreX, PreY, CurX, CurY : integer;
-    holst : tbitmap;
-    bufferHolst : tbitmap;
-    public
-      procedure BeforeDraw(x,y : integer); virtual;
-      procedure Draw(x,y : integer); virtual; abstract;
-  //    procedure AfterDraw(); virtual;
-      constructor create(holstToDraw : TBitmap);
+    PreX, PreY, CurX, CurY: integer;
+    holst: tbitmap;
+    bufferHolst: tbitmap;
+  public
+    procedure BeforeDraw(x, y: integer); virtual;
+    procedure Draw(x, y: integer); virtual; abstract;
+    constructor Create(holstToDraw: TBitmap);
+    procedure CtrlZ(); virtual; abstract;
+    procedure ZInit(Name: main; Pcolor, Bcolor: tcolor; TWid, X, Y: integer);
+      virtual; abstract;
+    procedure CoordInc(x, y: integer); virtual; abstract;
+    procedure Zshow(i: integer); virtual; abstract;
   end;
 
-   BufferRequired = class(main)
-     public
-       procedure Draw(x,y : integer);override; abstract;
-   end;
+  Zdata = record
+    NofTool: main;
+    PenColor: TColor;
+    BrushColor: Tcolor;
+    TWidth: integer;
+    Coord: array of integer;
+  end;
+
+  BufferRequired = class(main)
+  public
+    procedure Draw(x, y: integer); override; abstract;
+    procedure ZInit(Name: main; Pcolor, Bcolor: Tcolor; Twid, X, Y: integer); override;
+    procedure CoordInc(x, y: integer); override;
+    procedure Zshow(i: integer); override;
+  end;
 
 
-   BufferNotRequired = class(main)
-     public
+  BufferNotRequired = class(main)
+  public
+    procedure Draw(x, y: integer); override; abstract;
+    procedure ZInit(Name: main; Pcolor, Bcolor: tcolor; TWid, X, Y: integer); override;
+    procedure CoordInc(x, y: integer); override;
+    procedure Zshow(i: integer); override;
+  end;
+  //----------------------------------------//
+  Rectangle = class(BufferRequired)
+  public
+    procedure Draw(x, y: integer); override;
+  end;
 
-       procedure Draw(x,y : integer); override; abstract;
-   end;
-   //----------------------------------------//
+  Ellipse = class(BufferRequired)
+  public
+    procedure Draw(x, y: integer); override;
+  end;
 
-    Rectangle = class(BufferRequired)
-      public
-        procedure Draw(x,y : integer);override;
-    end;
+  Pen = class(BufferNotRequired)
+  public
+    procedure Draw(x, y: integer); override;
+  end;
 
-    Ellipse = class(BufferRequired)
-      public
-        procedure Draw(x,y : integer);override;
-    end;
+  Fill = class(BufferNotRequired)
+  public
+    procedure Draw(x, y: integer); override;
+    procedure BeforeDraw(x, y: integer); override;
+  end;
 
-    Pen = class(BufferNotRequired)
-      public
-        procedure Draw(x,y : integer); override;
-    end;
+  ReferenceClass = class of main;
 
-    ReferenceClass = class of main;
+  InstrName = record
+    instrument: ReferenceClass;
+    nameofinstr: string;
+  end;
 
-    InstrName = record
-      instrument : ReferenceClass;
-      nameofinstr : string;
-    end;
-
-    var ClassRef : array of InstrName;     //MAINVAR
+var
+  ClassRef: array of InstrName;     //MAINVAR
+  ZArr: array of Zdata;
 
 implementation
 
-  constructor main.create(holstToDraw : TBitmap);
+procedure BufferNotRequired.ZInit(Name: main; Pcolor, Bcolor: tcolor;
+  TWid, X, Y: integer);
+begin
+  SetLength(Zarr, length(Zarr) + 1);
+  with Zarr[high(Zarr)] do
   begin
-    holst := holstToDraw;
+    NofTool := Name;
+    PenColor := Pcolor;
+    BrushColor := Bcolor;
+    TWidth := Twid;
+    setlength(Coord, length(coord) + 2);
+    Coord[high(Coord) - 1] := X;
+    Coord[high(Coord) - 0] := Y;
   end;
+end;
 
-  procedure main.BeforeDraw(x,y : integer);
-  var r : trect;
+procedure BufferNotRequired.CoordInc(x, y: integer);
+begin
+  with ZArr[high(Zarr)] do
   begin
-    PreX := x;
-    PreY := y;
-    bufferHolst := tbitmap.create;
-    bufferholst.Width:= holst.Width;
-    bufferholst.Height:= holst.Height;
-    r := bounds(0,0,holst.Width,holst.Height);
-    bufferHolst.Canvas.CopyRect(rect(0,0,bufferHolst.Width,bufferHolst.Height),holst.canvas,r); {some procedure}
+    setlength(Coord, length(coord) + 2);
+    Coord[high(Coord) - 1] := X;
+    Coord[high(Coord) - 0] := Y;
   end;
+end;
 
-    procedure Rectangle.Draw(x,y : integer);
-    var r : trect;
+procedure BufferNotRequired.Zshow(i: integer);
+var
+  j: integer;
+begin
+  with ZArr[i] do
+  begin
+    holst.canvas.pen.color := PenColor;
+    holst.canvas.brush.color := BrushColor;
+    holst.canvas.pen.Width := TWidth;
+    NofTool.PreX := coord[0];
+    NofTool.PreY := coord[1];
+    j := 2;
+    while j <= high(coord) do
     begin
-      r := bounds(0,0,bufferholst.Width,bufferholst.Height);
-      holsT.Canvas.CopyRect(rect(0,0,holsT.Width,holsT.Height),bufferholst.canvas,r);
-      holst.Canvas.Rectangle(PreX,PreY,X,Y);
+      NofTool.Draw(coord[j], coord[j + 1]);
+      j += 2;
     end;
-
-    procedure Ellipse.Draw(x,y : integer);
-    var r : trect;
-    begin
-      r := bounds(0,0,bufferholst.Width,bufferholst.Height);
-      holsT.Canvas.CopyRect(rect(0,0,holsT.Width,holsT.Height),bufferholst.canvas,r);
-      holst.Canvas.Ellipse(PreX,PreY,x,y);
-    end;
-
-  procedure pen.Draw(x,y : integer);
-  begin
-    holst.canvas.line(PreX,PreY,x,y);
-    PreX := x;
-    PreY := y;
   end;
+end;
 
-  procedure Init(instrument : ReferenceClass; nameofinstr : string);
-  var i : integer;
+procedure BufferRequired.ZInit(Name: main; Pcolor, Bcolor: tcolor; TWid, X, Y: integer);
+begin
+  setlength(Zarr, length(Zarr) + 1);
+  with Zarr[high(Zarr)] do
   begin
-    setlength(ClassRef, length(ClassRef) + 1);
-    classref[high(ClassRef)].instrument := instrument;
-    classref[high(ClassRef)].nameofinstr := nameofinstr;
+    NofTool := Name;
+    PenColor := Pcolor;
+    BrushColor := Bcolor;
+    TWidth := Twid;
+    setlength(coord, 4);
+    Coord[0] := X;
+    Coord[1] := Y;
   end;
-   initialization
-     Init(pen, 'Pen');
-     Init(rectangle, 'Rectangle');
-     Init(ellipse, 'Ellipse');
+end;
+
+procedure BufferRequired.CoordInc(x, y: integer);
+begin
+  with ZArr[high(Zarr)] do
+  begin
+    Coord[2] := X;
+    Coord[3] := Y;
+  end;
+end;
+
+procedure BufferRequired.Zshow(i: integer);
+var
+  j: integer;
+begin
+  with ZArr[i] do
+  begin
+    holst.canvas.pen.color := PenColor;
+    holst.canvas.brush.color := BrushColor;
+    holst.canvas.pen.Width := TWidth;
+    NofTool.PreX := coord[0];
+    NofTool.PreY := coord[1];
+    NofTool.BeforeDraw(coord[0], coord[1]);
+    NofTool.Draw(coord[2], coord[3]);
+  end;
+end;
+
+constructor main.Create(holstToDraw: TBitmap);
+begin
+  holst := holstToDraw;
+end;
+
+procedure main.BeforeDraw(x, y: integer);
+var
+  r: trect;
+begin
+  PreX := x;
+  PreY := y;
+  bufferHolst := tbitmap.Create;
+  bufferholst.Width := holst.Width;
+  bufferholst.Height := holst.Height;
+  r := bounds(0, 0, holst.Width, holst.Height);
+  bufferHolst.Canvas.CopyRect(rect(0, 0, bufferHolst.Width, bufferHolst.Height),
+    holst.canvas, r); {some procedure}
+end;
+
+procedure Rectangle.Draw(x, y: integer);
+var
+  r: trect;
+begin
+  r := bounds(0, 0, bufferholst.Width, bufferholst.Height);
+  holsT.Canvas.CopyRect(rect(0, 0, holsT.Width, holsT.Height), bufferholst.canvas, r);
+  holst.Canvas.Rectangle(PreX, PreY, X, Y);
+end;
+
+procedure Ellipse.Draw(x, y: integer);
+var
+  r: trect;
+begin
+  r := bounds(0, 0, bufferholst.Width, bufferholst.Height);
+  holsT.Canvas.CopyRect(rect(0, 0, holsT.Width, holsT.Height), bufferholst.canvas, r);
+  holst.Canvas.Ellipse(PreX, PreY, x, y);
+end;
+
+procedure pen.Draw(x, y: integer);
+begin
+  holst.canvas.line(PreX, PreY, x, y);
+  PreX := x;
+  PreY := y;
+end;
+
+procedure fill.BeforeDraw(x, y: integer);
+begin
+  inherited BeforeDraw(x, y);
+  holst.canvas.FloodFill(x, y, holst.canvas.pen.color, TFillStyle.fsBorder);
+end;
+
+procedure fill.draw(x, y: integer);
+begin
+end;
+
+procedure Init(instrument: ReferenceClass; nameofinstr: string);
+var
+  i: integer;
+begin
+  setlength(ClassRef, length(ClassRef) + 1);
+  classref[high(ClassRef)].instrument := instrument;
+  classref[high(ClassRef)].nameofinstr := nameofinstr;
+end;
+
+initialization
+  Init(pen, 'Pen');
+  Init(fill, 'Fill');
+  Init(rectangle, 'Rectangle');
+  Init(ellipse, 'Ellipse');
 end.
-
--- mein.pas
+++ mein.pas
@@ -13,18 +13,19 @@ type
   { TMainForm }
 
   BrushStyleCbox = record
-    Name : string;
-    Style : TBrushStyle;
+    Name: string;
+    Style: TBrushStyle;
   end;
 
   TMainForm = class(TForm)
     BrushStyleCbox: TComboBox;
+    CTRLZButton: TButton;
     IncButton: TButton;
     DecButton: TButton;
     ColorDialog: TColorDialog;
     LineWidth: TEdit;
     MainMenu: TMainMenu;
-    MenuItem1: TMenuItem;
+    FileItem: TMenuItem;
     SaveAs: TMenuItem;
     CloseProgram: TMenuItem;
     OpenAs: TMenuItem;
@@ -37,6 +38,7 @@ type
     BrushColor: TShape;
     procedure ButtonClick(Sender: TObject);
     procedure BrushStyleCboxSelect(Sender: TObject);
+    procedure CTRLZButtonClick(Sender: TObject);
     procedure LineWidthChange(Sender: TObject);
     procedure LineWidthEditingDone(Sender: TObject);
     procedure IncButtonClick(Sender: TObject);
@@ -44,23 +46,22 @@ type
     procedure SaveAsClick(Sender: TObject);
     procedure CloseProgramClick(Sender: TObject);
     procedure OpenAsClick(Sender: TObject);
-    procedure ObjectMove(Obj : twincontrol; px,py,x,y : integer);
+    procedure ObjectMove(Obj: twincontrol; px, py, x, y: integer);
     procedure ToolsPanelMouseDown(Sender: TObject; Button: TMouseButton;
-      Shift: TShiftState; X, Y: Integer);
-    procedure ToolsPanelMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
+      Shift: TShiftState; X, Y: integer);
+    procedure ToolsPanelMouseMove(Sender: TObject; Shift: TShiftState; X, Y: integer);
     procedure ToolsPanelMouseUp(Sender: TObject; Button: TMouseButton;
-      Shift: TShiftState; X, Y: Integer);
+      Shift: TShiftState; X, Y: integer);
     procedure setbuttons();
     procedure FormCreate(Sender: TObject);
     procedure PaintBoxMouseDown(Sender: TObject; Button: TMouseButton;
-      Shift: TShiftState; X, Y: Integer);
-    procedure PaintBoxMouseMove(Sender: TObject; Shift: TShiftState; X,
-      Y: Integer);
+      Shift: TShiftState; X, Y: integer);
+    procedure PaintBoxMouseMove(Sender: TObject; Shift: TShiftState; X, Y: integer);
     procedure PaintBoxMouseUp(Sender: TObject; Button: TMouseButton;
-      Shift: TShiftState; X, Y: Integer);
+      Shift: TShiftState; X, Y: integer);
     procedure PaintBoxPaint(Sender: TObject);
     procedure PenColorMouseDown(Sender: TObject; Button: TMouseButton;
-      Shift: TShiftState; X, Y: Integer);
+      Shift: TShiftState; X, Y: integer);
   private
     { private declarations }
   public
@@ -69,11 +70,11 @@ type
 
 var
   MainForm: TMainForm;
-  holst : tbitmap;
-  isDrawing,isMoving : boolean;
-  px,py,cx,cy : integer;
-  tool : main;
-  BrushStyles : array of BrushStyleCbox;
+  holst: tbitmap;
+  isDrawing, isMoving: boolean;
+  px, py, cx, cy, history: integer;
+  tool: main;
+  BrushStyles: array of BrushStyleCbox;
 
 implementation
 
@@ -83,160 +84,187 @@ implementation
 
 procedure TMainForm.FormCreate(Sender: TObject);
 begin
- holst := tbitmap.Create;
- setbuttons();
- setStyles();
- tool := pen.create(holst);
- holst.Width := PaintBox.width;
- holst.Height := PaintBox.Height;
- holst.canvas.brush.color := $FFFFFF;
- holst.canvas.rectangle(0,0, holst.Width,holst.Height);
- holst.canvas.brush.Style:=bsClear;
+  holst := tbitmap.Create;
+  setbuttons();
+  setStyles();
+  tool := pen.Create(holst);
+  holst.Width := PaintBox.Width;
+  holst.Height := PaintBox.Height;
+  holst.canvas.brush.color := $FFFFFF;
+  holst.canvas.rectangle(0, 0, holst.Width, holst.Height);
+  holst.canvas.brush.Style := bsClear;
 end;
-
 //---------------------------PAINT-BOX------------------------------------------
 procedure TMainForm.PaintBoxMouseDown(Sender: TObject; Button: TMouseButton;
-  Shift: TShiftState; X, Y: Integer);
+  Shift: TShiftState; X, Y: integer);
 begin
- tool.BeforeDraw(x,y);
- isDrawing:=TRUE;
+  tool.BeforeDraw(x, y);
+  tool.ZInit(tool, holst.canvas.pen.color, holst.canvas.Brush.Color,
+    holst.canvas.pen.Width, X, Y);
+  isDrawing := True;
+  paintbox.invalidate;
 end;
 
-procedure TMainForm.PaintBoxMouseMove(Sender: TObject; Shift: TShiftState; X,
-  Y: Integer);
+procedure TMainForm.PaintBoxMouseMove(Sender: TObject; Shift: TShiftState;
+  X, Y: integer);
 begin
- if isDrawing = TRUE then begin
-  tool.Draw(x,y);
-  PaintBox.Invalidate;
- end;
+  if isDrawing = True then
+  begin
+    tool.Draw(x, y);
+    tool.CoordInc(x, y);
+    PaintBox.Invalidate;
+  end;
 end;
 
 procedure TMainForm.PaintBoxMouseUp(Sender: TObject; Button: TMouseButton;
-  Shift: TShiftState; X, Y: Integer);
+  Shift: TShiftState; X, Y: integer);
 begin
- isDrawing:=FALSE;
+  isDrawing := False;
 end;
 
 procedure TMainForm.PaintBoxPaint(Sender: TObject);
-var r : trect;
+var
+  r: trect;
 begin
- r := bounds(0,0,holst.Width,holst.Height);
- PaintBox.Canvas.CopyRect(rect(0,0,PaintBox.Width,PaintBox.Height),holst.canvas,r);
+  r := bounds(0, 0, holst.Width, holst.Height);
+  PaintBox.Canvas.CopyRect(rect(0, 0, PaintBox.Width, PaintBox.Height), holst.canvas, r);
 end;
-
 //----------------------PANEL-EVENTS--------------------------------------------
-procedure TMainForm.ObjectMove(Obj : Twincontrol; px,py,x,y : integer);
-var newx, newy : integer;
+procedure TMainForm.ObjectMove(Obj: Twincontrol; px, py, x, y: integer);
+var
+  newx, newy: integer;
 begin
   newx := obj.Left + x - px;
   newy := obj.top + y - py;
-  if (newx) >= (MainForm.Width - obj.width) then
-   obj.Left := MainForm.width - obj.width
+  if (newx) >= (MainForm.Width - obj.Width) then
+    obj.Left := MainForm.Width - obj.Width
   else if (newx) <= 0 then
-   obj.left := 0
+    obj.left := 0
   else
-   obj.Left := newx;
+    obj.Left := newx;
 
-  if (newy) >= (MainForm.height - obj.height) then
-   obj.top := MainForm.height - obj.height
+  if (newy) >= (MainForm.Height - obj.Height) then
+    obj.top := MainForm.Height - obj.Height
   else if (newy) <= 0 then
-   obj.top := 0
+    obj.top := 0
   else
-   obj.top := newy;
+    obj.top := newy;
 end;
 
 procedure TMainForm.ToolsPanelMouseDown(Sender: TObject; Button: TMouseButton;
-  Shift: TShiftState; X, Y: Integer);
+  Shift: TShiftState; X, Y: integer);
 begin
- isMoving := TRUE;
- px:=x;
- py:=y;
+  isMoving := True;
+  px := x;
+  py := y;
 end;
 
-procedure TMainForm.ToolsPanelMouseMove(Sender: TObject; Shift: TShiftState; X,
-  Y: Integer);
-var newx,newy : integer;
+procedure TMainForm.ToolsPanelMouseMove(Sender: TObject; Shift: TShiftState;
+  X, Y: integer);
+var
+  newx, newy: integer;
 begin
- if isMoving = TRUE then begin
-  ObjectMove(twincontrol(sender),px,py,x,y);
- end;
+  if isMoving = True then
+  begin
+    ObjectMove(twincontrol(Sender), px, py, x, y);
+  end;
 end;
 
 procedure TMainForm.ToolsPanelMouseUp(Sender: TObject; Button: TMouseButton;
-  Shift: TShiftState; X, Y: Integer);
+  Shift: TShiftState; X, Y: integer);
 begin
   isMoving := False;
 end;
-
 //-----------------------------TOOLS-PANEL-ITEM-EVENTS--------------------------
 procedure TMainForm.setbuttons();
-var i : integer;
-    button : tbutton;
+var
+  i: integer;
+  button: TButton;
 begin
-  for i := 0 to high(ClassRef) do begin
-    button := tbutton.Create(self);
+  for i := 0 to high(ClassRef) do
+  begin
+    button := TButton.Create(self);
     button.tag := i;
     button.Parent := ToolsPanel;
-    button.Width:=60;
-    button.Height:=20;
+    button.Width := 60;
+    button.Height := 20;
     button.top := 15 + 25 * i;
-    button.Visible:=TRUE;
-    button.Caption:=classref[i].nameofinstr;
-    button.Left:=10;
-    button.OnClick:=@ButtonClick;
+    button.Visible := True;
+    button.Caption := ClassRef[i].nameofinstr;
+    button.Left := 10;
+    button.OnClick := @ButtonClick;
   end;
 end;
 
 procedure TMainForm.ButtonClick(Sender: TObject);
 begin
-tool.free;
- tool := classref[tbutton(sender).tag].instrument.create(holst);
+  tool.Free;
+  tool := ClassRef[TButton(Sender).tag].instrument.Create(holst);
 end;
+//-----------------------------CTRL-Z-------------------------------------------
+procedure TMainForm.CTRLZButtonClick(Sender: TObject);
+var
+  i: integer;
+begin
+  holst.canvas.brush.color := $FFFFFF;
+  holst.canvas.brush.Style := bsSolid;
+  holst.canvas.rectangle(0, 0, holst.Width, holst.Height);
+  if high(zarr) - history > 0 then
+    history += 1;
+  ShowMessage(IntToStr(history));
+  for i := 0 to high(ZArr) - history do
+    tool.Zshow(i);
+  paintbox.invalidate;
+end;
+//-----------------------------CTRL-Y-------------------------------------------
 //-----------------------------OPTIONS-PANEL-ITEM-EVENTS------------------------
 procedure TMainForm.BrushStyleCboxSelect(Sender: TObject);
 begin
- holst.canvas.brush.Style:=brushstyles[BrushStyleCbox.ItemIndex].Style;
+  holst.canvas.brush.Style := brushstyles[BrushStyleCbox.ItemIndex].Style;
 end;
 
 procedure TMainForm.LineWidthChange(Sender: TObject);
 begin
-  if (LineWidth.text = '') OR (strtoint(LineWidth.text) < 1) then
-    LineWidth.text := '1';
-  if strtoint(LineWidth.text) > 99 then
-    LineWidth.text := '99';
+  if (LineWidth.Text = '') or (StrToInt(LineWidth.Text) < 1) then
+    LineWidth.Text := '1';
+  if StrToInt(LineWidth.Text) > 99 then
+    LineWidth.Text := '99';
 end;
 
 procedure TMainForm.LineWidthEditingDone(Sender: TObject);
 begin
-  holst.Canvas.pen.width := strtoint(LineWidth.text);
+  holst.Canvas.pen.Width := StrToInt(LineWidth.Text);
 end;
 
 procedure TMainForm.IncButtonClick(Sender: TObject);
 begin
-   LineWidth.text := inttostr(strtoint(LineWidth.text) + tbutton(sender).Tag);
-   LineWidthEditingDone(LineWidth);
+  LineWidth.Text := IntToStr(StrToInt(LineWidth.Text) + TButton(Sender).Tag);
+  LineWidthEditingDone(LineWidth);
 end;
 
 procedure TMainForm.SetStyles();
-var i : integer;
+var
+  i: integer;
 begin
-  for i := 0 to high(BrushStyles) do begin
-    BrushStyleCbox.Items.Add(brushstyles[i].name);
+  for i := 0 to high(BrushStyles) do
+  begin
+    BrushStyleCbox.Items.Add(brushstyles[i].Name);
   end;
 end;
 
-procedure InitStyle(NM : string;STL : TBrushStyle);
+procedure InitStyle(NM: string; STL: TBrushStyle);
 begin
   setlength(BrushStyles, length(BrushStyles) + 1);
-  with BrushStyles[high(BrushStyles)] do begin
-    name := nm;
+  with BrushStyles[high(BrushStyles)] do
+  begin
+    Name := nm;
     style := stl;
   end;
 end;
 //-----------------------------DIALOGS----------------------------------------
 procedure TMainForm.SaveAsClick(Sender: TObject);
 begin
-  if SavePictureDialog.execute then
+  if SavePictureDialog.Execute then
     holst.SaveToFile(SavePictureDialog.filename);
 end;
 
@@ -247,22 +275,27 @@ end;
 
 procedure TMainForm.OpenAsClick(Sender: TObject);
 begin
-  if OpenPictureDialog1.execute then
-     holst.LoadFromFile(OpenPictureDialog1.FileName);
+  if OpenPictureDialog1.Execute then
+    holst.LoadFromFile(OpenPictureDialog1.FileName);
   PaintBox.Invalidate;
 end;
 
 procedure TMainForm.PenColorMouseDown(Sender: TObject; Button: TMouseButton;
-  Shift: TShiftState; X, Y: Integer);
+  Shift: TShiftState; X, Y: integer);
 begin
-  if ColorDialog.execute then
-    tshape(sender).Brush.color := ColorDialog.Color;
+  if ColorDialog.Execute then
+    tshape(Sender).Brush.color := ColorDialog.Color;
   holst.canvas.pen.color := PenColor.brush.color;
   holst.canvas.brush.color := BrushColor.brush.color;
 end;
 
 initialization
-   InitStyle('Horizontal', bsHorizontal);
-   InitStyle('Solid', bsSolid);
+  InitStyle('Horizontal', bsHorizontal);
+  InitStyle('Solid', bsSolid);
+  InitStyle('Clear', bsClear);
+  InitStyle('Vertical', bsVertical);
+  InitStyle('FDiagonal', bsFdiagonal);
+  InitStyle('BDiagonal', bsBDiagonal);
+  InitStyle('Cross', bsCross);
+  InitStyle('DiagCross', bsDiagCross);
 end.
-
